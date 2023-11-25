8fee4720791addd43bd761cb04c633f9cbbd2adf
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-11-25 15:50:55 -0800

    [Chat App] backend - connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // configurations
 const app = express();
 dotenv.config();
+connectDB();
 
 // routes
 app.get("/", (req, res) => {
@@ -27,5 +30,5 @@ app.get("/api/chat/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`.yellow.bold);
 });
