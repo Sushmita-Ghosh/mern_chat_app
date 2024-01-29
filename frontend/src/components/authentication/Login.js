0373b302e04c28d6ba7bec38d5162235dab8a443
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-01-28 21:50:55 -0800

    [Chat App] Created Login oN submit functionality

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -6,17 +6,80 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => setShow(!show);
-  const postDetails = (pics) => {};
-  const submitHandler = () => {};
+  const toast = useToast();
+  const history = useHistory();
+
+  const submitHandler = async () => {
+    setLoading(true);
+    // if all fields are not filled show error to user
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    // make request to login
+    try {
+      // declaring header as it is json format
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      // sending data in json format to backend
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      // if successful - then show toast notification to user
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      // store data in local storage
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+
+      // redirect to chat page
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
@@ -25,6 +88,7 @@ const Login = () => {
           <FormLabel>Email</FormLabel>
           <Input
             placeholder="Enter your email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </FormControl>
@@ -33,6 +97,7 @@ const Login = () => {
           <InputGroup size="md">
             <Input
               type={show ? "text" : "password"}
+              value={password}
               placeholder="Enter Password"
               onChange={(e) => setPassword(e.target.value)}
             />
@@ -48,6 +113,7 @@ const Login = () => {
           width="100%"
           style={{ marginTop: 15 }}
           onClick={submitHandler}
+          isLoading={loading}
         >
           Login
         </Button>
